dbacd73357109d8c2b94991272cb5744d7eb9696
Emily Tang <emilyyunq@gmail.com>
2025-02-15 13:33:21 +0800

    Added animation

--- player.gd
+++ player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-
+@onready var anim_player = $AnimatedSprite2D # 引用 Sprite 节点
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -17,9 +17,17 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			anim_player.flip_h = false
+		elif direction < 0:
+			anim_player.flip_h = true
+		if is_on_floor:
+			anim_player.play("walk")
+		print(is_on_floor())
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		anim_player.play("idle")
+		
 	move_and_slide()
