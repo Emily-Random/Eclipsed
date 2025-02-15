d69b65b62fddc9f638debea2fb6ca512e9a9a910
Emily Tang <emilyyunq@gmail.com>
2025-02-15 15:23:12 +0800

    updates

--- player.gd
+++ player.gd
@@ -6,10 +6,12 @@ const JUMP_VELOCITY = -400.0
 @onready var anim_player = $AnimatedSprite2D # 引用 Sprite 节点
 	
 func _physics_process(delta: float) -> void:
+	print("Velocity: ", velocity)
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		velocity.y += 980 * delta
+		print("Falling! Velocity.y:", velocity.y)
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -28,5 +30,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		anim_player.play("idle")
-		
+	print("Moving! Velocity:", velocity)
 	move_and_slide()
