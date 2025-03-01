7af0b8a7403f7c41636b8a4d76bc7d6c0c2e5c17
Emily Tang <emilyyunq@gmail.com>
2025-03-01 17:53:30 +0800

    With shadow features.

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 60.0
+const JUMP_VELOCITY = -200.0
 
 @onready var anim_player = $AnimatedSprite2D # Reference to the AnimatedSprite2D node
 
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+	
 	# Get horizontal input
 	var direction := Input.get_axis("Left", "Right")
 
